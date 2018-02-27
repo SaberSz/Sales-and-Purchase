a44d1046291a4108e8ade4b0e888ea059b870f99
dylan <dylan@192.168.0.103>
2018-02-27 23:54:48 +0530

    Sql file updated. Due date added in invoice pane.

--- src/main/java/com/mycompany/snp/SalesController.java
+++ src/main/java/com/mycompany/snp/SalesController.java
@@ -192,6 +192,8 @@ public class SalesController implements Initializable {
     private JFXComboBox<String> inv_invbox;
     @FXML
     private Label inv_tick;
+    @FXML
+    private JFXDatePicker due_date;
 
 
     /**
@@ -1748,6 +1750,44 @@ public class SalesController implements Initializable {
 
     @FXML
     private void Invoice_Save_Button_Clicked_inInvPane(MouseEvent event) {
+        //due_date
+        /*
+            @FXML
+    private JFXComboBox<Integer> inv_pno;
+
+    private Label pno_tick;
+
+    private JFXComboBox<String> inv_invbox;
+
+    private Label inv_tick;
+
+    private JFXDatePicker due_date;
+
+    private JFXTextField inv_no;
+
+    private JFXTextField inv_cmp;
+
+    private JFXTextField inv_tum;
+
+    private JFXTextField inv_qno;
+
+    private JFXTextField inv_po;
+
+    private JFXTextField inv_sp;
+
+    private JFXTextField inv_acc;
+
+    private JFXTextArea inv_to;
+
+    private TableView<Person3> inv_newtable;
+
+    private JFXTextField inv_gst;
+
+    private JFXTextField inv_total;
+
+    private JFXTextField inv_amt;
+        
+        */
     }
 
     @FXML
@@ -2015,14 +2055,10 @@ public class SalesController implements Initializable {
                String nme=r.getString("Name");
                String em=r.getString("email");
                String phno=r.getString("phone");
-               inv_to.setText("Name:"+nme+"\n"+"Address:"+add+"\n"+"Email:"+em+"\n"+"Phone NO:"+phno);                   
+               inv_to.setText(nme+"\n\n"+add+"\n\n"+em+"\n\n"+phno);                   
           }catch(Exception e){
               Logger.getLogger(SalesController.class.getName()).log(Level.SEVERE, null, e);
           }
-          
-          
-        
-        
         inv_newtable.getColumns().clear();
         inv_newtable.getItems().clear();
         ObservableList<Person3> data;    
@@ -2078,6 +2114,11 @@ public class SalesController implements Initializable {
         
     }
 
+    void clear_in_invpane(){
+        
+        
+        
+    }
     @FXML
     private void inv_edit_hit(MouseEvent event) {
        inv_pno.setDisable(true);
@@ -2089,6 +2130,7 @@ public class SalesController implements Initializable {
     inv_tick.setVisible(true);
     inv_pno.setVisible(false);
     pno_tick.setVisible(false);
+    
        
        
     
